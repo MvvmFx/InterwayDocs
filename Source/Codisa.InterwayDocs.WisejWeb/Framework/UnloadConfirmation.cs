4fcdb8dc2ec6e59ca0f86bf90f9b92b519c11c00
tfreitasleal <tfreitasleal@gmail.com>
2018-04-18 10:49:42 +0100

    Add BrowserUnload.js, UnloadConfirmation.cs (#100)

---
+++ Source/Codisa.InterwayDocs.WisejWeb/Framework/UnloadConfirmation.cs
@@ -0,0 +1,52 @@
+﻿using Application = Wisej.Web.Application;
+
+namespace Codisa.InterwayDocs.Framework
+{
+    public static class UnloadConfirmation
+    {
+        static UnloadConfirmation()
+        {
+            PrivateBeforeUnloadMessage = string.Empty;
+        }
+
+        private static string PrivateBeforeUnloadMessage
+        {
+            get { return Application.Session.BeforeUnloadMessage; }
+            set { Application.Session.BeforeUnloadMessage = value; }
+        }
+
+        public static string BeforeUnloadMessage
+        {
+            get { return PrivateBeforeUnloadMessage; }
+            set
+            {
+                if (PrivateBeforeUnloadMessage != value)
+                {
+                    if (value == null)
+                        PrivateBeforeUnloadMessage = string.Empty;
+                    else
+                        PrivateBeforeUnloadMessage = value;
+
+                    SetBeforeUnloadMessage();
+                }
+            }
+        }
+
+        private static void SetBeforeUnloadMessage()
+        {
+            if (string.IsNullOrWhiteSpace(PrivateBeforeUnloadMessage))
+            {
+                Application.Call("window.disableUnloadConfirmation");
+            }
+            else
+            {
+                Application.Call("window.enableUnloadConfirmation", PrivateBeforeUnloadMessage);
+            }
+        }
+
+        public static void Restore()
+        {
+            SetBeforeUnloadMessage();
+        }
+    }
+}
