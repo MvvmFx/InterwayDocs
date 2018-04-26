3fbb4b2e120951ab62b09ba33de5e5dce4f96109
tfreitasleal <tfreitasleal@gmail.com>
2018-04-26 10:35:29 +0100

    Cosmetic

--- Source/Codisa.InterwayDocs.WisejWeb/Properties/AssemblyInfo.cs
+++ Source/Codisa.InterwayDocs.WisejWeb/Properties/AssemblyInfo.cs
@@ -1,11 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Wisej.Core;
 
 // Add the WisejResources attribute to 
 // merge js and css embedded resources with the wisej package.
-// [assembly: WisejResources(ExcludeList: "")]
+[assembly: WisejResources(ExcludeList: "")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,7 +13,3 @@ using Wisej.Core;
 [assembly: AssemblyTitle("Wisej Web UI for .NET 4.5")]
 [assembly: AssemblyProduct("InterwayDocs")]
 [assembly: AssemblyDescription("Wisej Web UI for .NET 4.5")]
-
-// Make sure that you have this line uncommented
-// so that your JavaScript file will be sent to the client
-[assembly: WisejResources(ExcludeList: "")]
