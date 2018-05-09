dcb4c00c400ca76e8587936d0850464f7f612b3c
tfreitasleal <tfreitasleal@gmail.com>
2018-05-09 14:40:34 +0100

    Add Project to read ResX files and save them to the database. (#116)

--- Source/sprocs/ResourceMigration.sql
+++ Source/sprocs/ResourceMigration.sql
@@ -1,85 +1,9 @@
-/****** Object:  StoredProcedure [dbo].[GetResourceTranslationsByCulture] ******/
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetResourceTranslationsByCulture]') AND type in (N'P', N'PC'))
-    DROP PROCEDURE [dbo].[GetResourceTranslationsByCulture]
+/****** Object:  StoredProcedure [dbo].[MigrationAddResource] ******/
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MigrationAddResource]') AND type in (N'P', N'PC'))
+    DROP PROCEDURE [dbo].[MigrationAddResource]
 GO
 
-CREATE PROCEDURE [dbo].[GetResourceTranslationsByCulture]
-    @UICulture varchar(5)
-AS
-    BEGIN
-
-        SET NOCOUNT ON
-
-        /* Get ResourceInfo from table */
-        SELECT
-            [Resources].[ResourceType],
-            [Resources].[ResourceName],
-            [ResourceTranslations].[UICulture],
-            [ResourceTranslations].[Translation]
-        FROM [dbo].[ResourceTranslations]
-            INNER JOIN [dbo].[Resources] ON [ResourceTranslations].[ResourceId] = [Resources].[ResourceId]
-        WHERE
-            [ResourceTranslations].[UICulture] = @UICulture
-        ORDER BY
-            [Resources].[ResourceType], [Resources].[ResourceName]
-
-    END
-GO
-
-/****** Object:  StoredProcedure [dbo].[GetAllResources] ******/
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetAllResources]') AND type in (N'P', N'PC'))
-    DROP PROCEDURE [dbo].[GetAllResources]
-GO
-
-CREATE PROCEDURE [dbo].[GetAllResources]
-AS
-    BEGIN
-
-        SET NOCOUNT ON
-
-        /* Get Resource from table */
-        SELECT
-            [Resources].[ResourceId],
-            [Resources].[ResourceType],
-            [Resources].[ResourceName]
-        FROM [dbo].[Resources]
-        ORDER BY
-            [Resources].[ResourceType], [Resources].[ResourceName]
-
-    END
-GO
-
-/****** Object:  StoredProcedure [dbo].[GetResourceTranslations] ******/
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GetResourceTranslations]') AND type in (N'P', N'PC'))
-    DROP PROCEDURE [dbo].[GetResourceTranslations]
-GO
-
-CREATE PROCEDURE [dbo].[GetResourceTranslations]
-    @ResourceId int
-AS
-    BEGIN
-
-        SET NOCOUNT ON
-
-        /* Get ResourceTranslation from table */
-        SELECT
-            [ResourceTranslations].[UICulture],
-            [ResourceTranslations].[Translation]
-        FROM [dbo].[ResourceTranslations]
-        WHERE
-            [ResourceTranslations].[ResourceId] = @ResourceId
-        ORDER BY
-            [ResourceTranslations].[UICulture]
-
-    END
-GO
-
-/****** Object:  StoredProcedure [dbo].[AddResource] ******/
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddResource]') AND type in (N'P', N'PC'))
-    DROP PROCEDURE [dbo].[AddResource]
-GO
-
-CREATE PROCEDURE [dbo].[AddResource]
+CREATE PROCEDURE [dbo].[MigrationAddResource]
     @ResourceId int OUTPUT,
     @ResourceType varchar(50),
     @ResourceName varchar(50)
@@ -106,12 +30,12 @@ AS
     END
 GO
 
-/****** Object:  StoredProcedure [dbo].[AddResourceTranslation] ******/
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddResourceTranslation]') AND type in (N'P', N'PC'))
-    DROP PROCEDURE [dbo].[AddResourceTranslation]
+/****** Object:  StoredProcedure [dbo].[MigrationAddResourceTranslation] ******/
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MigrationAddResourceTranslation]') AND type in (N'P', N'PC'))
+    DROP PROCEDURE [dbo].[MigrationAddResourceTranslation]
 GO
 
-CREATE PROCEDURE [dbo].[AddResourceTranslation]
+CREATE PROCEDURE [dbo].[MigrationAddResourceTranslation]
     @ResourceId int,
     @UICulture varchar(5),
     @Translation varchar(500)
@@ -142,49 +66,3 @@ GO
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateResource]') AND type in (N'P', N'PC'))
     DROP PROCEDURE [dbo].[UpdateResource]
 GO
-
-CREATE PROCEDURE [dbo].[UpdateResource]
-    @ResourceId int,
-    @ResourceType varchar(50),
-    @ResourceName varchar(50)
-AS
-    BEGIN
-
-        SET NOCOUNT ON
-
-        /* Update object in dbo.Resources */
-        UPDATE [dbo].[Resources]
-        SET
-            [ResourceType] = @ResourceType,
-            [ResourceName] = @ResourceName
-        WHERE
-            [ResourceId] = @ResourceId
-
-    END
-GO
-
-/****** Object:  StoredProcedure [dbo].[UpdateResourceTranslation] ******/
-IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[UpdateResourceTranslation]') AND type in (N'P', N'PC'))
-    DROP PROCEDURE [dbo].[UpdateResourceTranslation]
-GO
-
-CREATE PROCEDURE [dbo].[UpdateResourceTranslation]
-    @ResourceId int,
-    @UICulture varchar(5),
-    @Translation varchar(500)
-AS
-    BEGIN
-
-        SET NOCOUNT ON
-
-        /* Update object in dbo.Resources */
-        UPDATE [dbo].[ResourceTranslations]
-        SET
-            [Translation] = @Translation
-        WHERE
-            [ResourceId] = @ResourceId AND
-            [UICulture] = @UICulture
-
-    END
-GO
-
